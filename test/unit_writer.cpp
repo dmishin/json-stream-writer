21e2dcd1a40a6725dcb34759daef408028a1d984
Dmishin <shintyakov@gmail.com>
2012-01-15 19:51:34 +0400

    Tests added: Validation

--- test/unit_writer.cpp
+++ test/unit_writer.cpp
@@ -67,14 +67,14 @@ TEST( Arrays, General ){
   JsonWriter w(s);
 
   w.begin_array();
-  EXPECT_EQ( w.level(), 1 );
+  EXPECT_EQ( w.level(), size_t(1) );
   w.value(-1);
   w.value("hello");
   w.value("");
   w.value_null();
   w.value(false);
   w.end_array();
-  EXPECT_EQ( w.level(), 0 );
+  EXPECT_EQ( w.level(), 0u );
 
   EXPECT_EQ( s.str(), "[-1,\"hello\",\"\",null,false]" );
 }
@@ -86,7 +86,7 @@ TEST( Arrays, Empty ){
   w.begin_array();
   w.end_array();
   EXPECT_EQ( s.str(), "[]");
-  EXPECT_EQ( w.level(), 0 );
+  EXPECT_EQ( w.level(), 0u );
 }
 
 TEST( Objects, Empty ){
@@ -94,9 +94,9 @@ TEST( Objects, Empty ){
   stringstream s;
   JsonWriter w(s);
   w.begin_object();
-  EXPECT_EQ( w.level(), 1 );
+  EXPECT_EQ( w.level(), 1u );
   w.end_object();
-  EXPECT_EQ( w.level(), 0 );
+  EXPECT_EQ( w.level(), 0u );
   EXPECT_EQ( s.str(), "{}" );
 }
 
@@ -117,4 +117,147 @@ TEST( Objects, General ){
   w.end_object();
   
   EXPECT_EQ( s.str(), "{\"a\":\"hello\",\"b\":1000,\"c\":null,\"d\":true}" );
+}
+
+TEST( Writer, NestedObjects ){
+  using namespace std;
+  stringstream s;
+  JsonWriter w(s);
+
+  /* Trying to generate this:
+     [{"fld":10,
+       "arr":[{},2,3,4]},
+      "test",
+      []]
+  */
+  string original = "[{\"fld\":10,\"arr\":[{},2,3,4]},\"test\",[]]";
+
+  w.begin_array();{
+    EXPECT_EQ( w.level(), 1u );
+    w.begin_object();{
+      EXPECT_EQ( w.level(), 2u );
+      w.name("fld");
+      w.value( 10 );
+      w.name("arr");
+      w.begin_array();{
+	w.begin_object();
+	w.end_object();
+	w.value(2);
+	w.value(3);
+	w.value(4);
+      };w.end_array();
+    };w.end_object();
+    w.value("test");
+    w.begin_array();
+    w.end_array();
+  };w.end_array();
+  EXPECT_EQ( w.level(), 0u );
+  EXPECT_EQ( s.str(), original );
+}
+
+TEST( Writer, ArrowAPI ){
+  using namespace std;
+  stringstream s;
+  JsonWriter w(s);
+  //Test for more concise iostream-like API with "<<"
+  w << begin_array << 1 << 2 << 3
+    << begin_object 
+    << Name("field1") << 10
+    << Name("field2") << true
+    << Name("array_field") 
+    << begin_array
+    << "the" << "array" << "sample"
+    << end_array
+    << end_object
+    << end_array;
+  //expected json:
+  //[1,2,3,{"field1":10,"field2":true,"array_field":["the","array","sample"]}]
+  EXPECT_EQ( s.str(), "[1,2,3,{\"field1\":10,\"field2\":true,\"array_field\":[\"the\",\"array\",\"sample\"]}]" );
+}
+
+
+TEST( Validation, WriteAfterFinish ){
+  using namespace std;
+  stringstream s;
+  JsonWriter w(s);
+
+  w.value( "hello" );
+  ASSERT_THROW( w.value("after-end"), JsonWriterStateError );
+
+  w.reset();
+  w.value( "hello" );
+  ASSERT_THROW( w.value_null(), JsonWriterStateError );
+
+  w.reset();
+  w.value( "hello" );
+  ASSERT_THROW( w.begin_object(), JsonWriterStateError );
+
+  w.reset();
+  w.value( "hello" );
+  ASSERT_THROW( w.begin_array(), JsonWriterStateError );
+
+  w.reset();
+  w.value( "hello" );//attempt to write unexpected name
+  ASSERT_THROW( w.name("field"), JsonWriterStateError );
+}
+TEST( Validation, AtInitialState ){
+  using namespace std;
+  stringstream s;
+  JsonWriter w(s);
+  ASSERT_THROW( w.name("some field"), JsonWriterStateError );
+
+  w.reset();
+  ASSERT_THROW( w.end_object(), JsonWriterStateError );
+  w.reset();
+  ASSERT_THROW( w.end_array(), JsonWriterStateError );
+}
+TEST( Validation, InsideArray ){
+  using namespace std;
+  stringstream s;
+  JsonWriter w(s);
+  w.begin_array();
+  ASSERT_THROW( w.name("some field"), JsonWriterStateError );
+
+  w.reset();
+  w.begin_array();
+  ASSERT_THROW( w.end_object(), JsonWriterStateError );
+}
+TEST( Validation, InsideObject ){
+  using namespace std;
+  stringstream s;
+  JsonWriter w(s);
+  w.begin_object();
+  ASSERT_THROW( w.value( 1 ), JsonWriterStateError ); //Value withut name in theobject
+
+  w.reset();
+  w.begin_object();
+  ASSERT_THROW( w.value_null(), JsonWriterStateError );
+
+  w.reset();
+  w.begin_object();
+  ASSERT_THROW( w.begin_array(), JsonWriterStateError );
+
+  w.reset();
+  w.begin_object();
+  ASSERT_THROW( w.end_array(), JsonWriterStateError );
+
+  w.reset();
+  w.begin_object();
+  ASSERT_THROW( w.begin_object(), JsonWriterStateError );
+
+}
+
+TEST( Validation, ObjectField ){
+  using namespace std;
+  stringstream s;
+  JsonWriter w(s);
+
+  w.begin_object();w.name("field");
+  ASSERT_THROW( w.name("anouther name"), JsonWriterStateError ); //name after name
+  w.reset();
+  w.begin_object();w.name("field");
+  ASSERT_THROW( w.end_object(), JsonWriterStateError ); //name without value
+  w.reset();
+  w.begin_object();w.name("field");
+  ASSERT_THROW( w.end_array(), JsonWriterStateError ); //unexpected end
 }
